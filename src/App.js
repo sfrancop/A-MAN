55c622b759f26696e806b6a2e1251235dc577565
Sergio Franco <s.francop@uniandes.edu.co>
2022-11-23 15:12:42 -0500

    App in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import Ingreso from './pages/Ingreso/Ingreso';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Ingreso from "./pages/Ingreso/Ingreso";
+import Registro from "./pages/Registro/Registro";
+import Hola from "./pages/Hola/Hola";
+import Filtros from "./pages/Filtros/Filtros";
+import Restaurantes from "./pages/Restaurantes/Restaurantes";
+import Restaurante from "./pages/Restaurante/Restaurante";
 
 function App() {
   return (
     <div className="App">
-      <Ingreso/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Ingreso />}/>
+        <Route path="/registro" element={<Registro />}/>
+        <Route path="/hola" element={<Hola />}/>
+        <Route path="/filtros" element={<Filtros />}/>
+        <Route path="/restaurantes" element={<Restaurantes />}/>
+        <Route path='/restaurantes/:id' element={<Restaurante />}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
 
-export {App}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Ingreso from "./pages/Ingreso/Ingreso";
-import Registro from "./pages/Registro/Registro";
-import Hola from "./pages/Hola/Hola";
-import Filtros from "./pages/Filtros/Filtros";
-import Restaurantes from "./pages/Restaurantes/Restaurantes";
-import Restaurante from "./pages/Restaurante/Restaurante";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from "./App"
 import reportWebVitals from './reportWebVitals';
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
 root.render(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Ingreso />}/>
-    <Route path="/registro" element={<Registro />}/>
-    <Route path="/hola" element={<Hola />}/>
-    <Route path="/filtros" element={<Filtros />}/>
-    <Route path="/restaurantes" element={<Restaurantes />}/>
-    <Route path='/restaurantes/:id' element={<Restaurante />}/>
-  </Routes>
-</BrowserRouter>
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
