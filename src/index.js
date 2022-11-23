feed05f748833f418ab8263ac662067b4b541175
Sergio Franco <s.francop@uniandes.edu.co>
2022-11-23 15:59:20 -0500

    test4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from "./App"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Ingreso from "./pages/Ingreso/Ingreso";
 import Registro from "./pages/Registro/Registro";
